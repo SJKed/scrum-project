9e2a827167308b6ea36d4788c2cf3b8ab9815a69
SJKed <simonjurstedtskolan@gmail.com>
2022-02-09 14:18:39 +0100

    Spring cleaning the documents

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>This is the about page</p>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
