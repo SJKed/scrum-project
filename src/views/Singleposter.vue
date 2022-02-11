ef01359cc66ee6a47d159124093d331e0ec75dae
Nattzor <74113960+Nattzor@users.noreply.github.com>
2022-02-11 08:21:18 +0100

    test

--- src/views/Singleposter.vue
+++ src/views/Singleposter.vue
@@ -1,12 +1,20 @@
 <template>
-  <h1>{{poster}}</h1>
+  <article class="poster" :class="{large}">
+    <div class="poster-cover">
+      <h2>{{ poster.title }}</h2>
+    </div>
+  </article>
 </template>
-
 <script>
+import posters from "../assets/data.js"
 export default {
-  data() { return {
-    id: 1
-  }
+  data() {  return {
+      productData: [...posters],
+      productId: this.$route.params.id,
+    }
+  },
+  props: {
+    large: Boolean
   },
 computed: {
   poster() {
@@ -17,5 +25,21 @@ computed: {
 </script>
 
 <style>
-
+.product{
+  color: black;
+  width: 250px;
+  height: 350px;
+  display: flex;
+  flex-direction: column-reverse;
+}
+.product.large{
+  width: 450px;
+  height: 680px;
+}
+.poster-cover {
+  width: 13rem;
+  height: 15rem;
+  background-color: aquamarine;
+  margin: 1rem;
+}
 </style>
